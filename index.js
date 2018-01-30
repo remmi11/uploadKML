b6398ee71b99dd08448bdaf79d5e2169f94cd78d
remmi11 <wtgeographer@gmail.com>
2018-01-29 21:09:00 -0600

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const promise = require('bluebird'); // or any other Promise/A+ compatible library;
+const promise = require('bluebird'); 
 var tj = require('togeojson');
 var fs = require('fs');
 
@@ -11,7 +11,9 @@ const initOptions = {
 
 const pgp = require('pg-promise')(initOptions);
 
-// Database connection details;
+// Database connection details
+// Swap out with own db credentials
+// create table with setup.sql
 const cn = {
     host: 'nutty-blueberry.db.elephantsql.com', // 'localhost' is the default;
     port: 5432, // 5432 is the default;
@@ -24,17 +26,15 @@ const db = pgp(cn); // database instance;
 
 // convert kml to geojson
 var kml = new DOMParser().parseFromString(fs.readFileSync('map.kml', 'utf8'));
-
 var converted = tj.kml(kml);
-
 var convertedWithStyles = tj.kml(kml, { styles: true });
-
 var myJSON = JSON.stringify(convertedWithStyles);
 
+//extract geometries from feature collection
 var myGeometries = myJSON.slice(69, -2);
 
+// prepare and execute insert query
 var myQuery = "INSERT INTO kmlTable(id,geom) values(1,ST_SetSRID(ST_GeomFromGeoJSON(" + "'" +  myGeometries + "'"+ "),4326))"
-
 db.any(myQuery)
     .then(data => {
         console.log('DATA:', data); // print data;
